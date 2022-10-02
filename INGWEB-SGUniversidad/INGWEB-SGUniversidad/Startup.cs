6c76ad1e8b6f8fa15eac729f204b93543eab98e7
Esteban Sampedro <sebasgar11@gmail.com>
2022-10-02 18:44:12 -0500

    Nuevos cambios

--- INGWEB-SGUniversidad/INGWEB-SGUniversidad/Startup.cs
+++ INGWEB-SGUniversidad/INGWEB-SGUniversidad/Startup.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Serialization;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 namespace INGWEB_SGUniversidad
 {
@@ -58,6 +60,13 @@ namespace INGWEB_SGUniversidad
             {
                 endpoints.MapControllers();
             });
+            
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                   Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+                RequestPath = "/Photos"
+            })
         }
     }
 }
