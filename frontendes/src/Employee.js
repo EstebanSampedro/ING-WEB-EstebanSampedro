df0f6cf29c6ff880b43579723102180932e00ae7
Esteban Sampedro <sebasgar11@gmail.com>
2022-10-04 14:43:08 -0500

    FrontEnd updates
    new frontend updates

--- frontendes/src/Employee.js
+++ frontendes/src/Employee.js
@@ -270,8 +270,8 @@ export class Employee extends Component{
 
      </div>
      <div className="p-2 w-50 bd-highlight">
-         <img width="250px" height="250px"
-         src={PhotoPath+PhotoFileName}/>
+         <img 
+         src={PhotoPath+PhotoFileName} alt=""/>
          <input className="m-2" type="file" onChange={this.imageUpload}/>
      </div>
     </div>
--- frontendes/src/Home.js
+++ frontendes/src/Home.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';
+import logo from './images/sistema2.png'
 
 export class Home extends Component{
     render(){
         return(
-            <div>
-                <h3>This is Home page</h3>
+            <div className='Inicio'>
+                <img src={logo} alt="inicialLogo"/>
+                <h3>Gestiona tu personal de la manera más óptima posible!</h3>
             </div>
         )
     }
